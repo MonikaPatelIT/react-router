90ac9179627b605c35944f0df98a76d37f611b42
Monika Patel <monikapatel.it@outlook.com>
2019-01-23 21:57:29 +1300

    Added react router app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import About from './Components/About';
+import Navigation from './Components/Navigation';
+import Error from './Components/Error';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <BrowserRouter>
+<div>
+<Navigation></Navigation>
+<Switch>
+          <Route path="/" component={Home} exact ></Route>
+          <Route path="/About" component={About}></Route>
+          <Route path="/Contact" component={Contact}></Route>
+          <Route  component={Error}></Route>
+        </Switch>
+        
+</div>
+        
+        </BrowserRouter>
       </div>
     );
   }
---
+++ src/Components/Error.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Error = () => {
+return (<div>
+    <p>Error: 404 this page not found </p>
+</div>
+
+);
+
+};
+
+export default Error;
+
+
+
